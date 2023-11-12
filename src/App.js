8e6c84462a25ab6661fcb6717df72accd3672c50
eva <evaw0929@gmail.com>
2023-11-12 16:47:27 -0500

    lab09

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="blue-frame">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <h3>101381503</h3>
+        <h4>Yiting Wang</h4>
+        <h5>George Brown College, Toronto</h5>
+      </div>
+    </div>
+  );
+}
+
+export default App;
